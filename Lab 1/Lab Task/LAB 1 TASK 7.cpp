6488e39ec167554654071bb9e3efb8e0e9ce8d12
anasabbas575 <anasabbas503@gmail.com>
2026-03-04 07:17:27 +0500

    Update LAB 1 TASK 7.cpp

--- Lab 1/Lab Task/LAB 1 TASK 7.cpp
+++ Lab 1/Lab Task/LAB 1 TASK 7.cpp
@@ -1,7 +1,7 @@
-// Name: Abdul Ahad
-// Department: Electrical Engineering (AI)
-// Registration No: BF25NWELE0692
-// Section: B
+// Name: Anus Abbas Khan
+// Department: Electrical Engineering (power)
+// Registration No: BF25NWELE0723
+// Section: A
 
 
 #include <iostream>// include library for input and output
@@ -15,4 +15,5 @@ int main()// it is sttarting point of program
     
     return 0;   // indicate that program ended successfully
     } // end function main
+
 

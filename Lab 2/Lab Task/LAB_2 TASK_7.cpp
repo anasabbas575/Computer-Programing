e5f7523c218447d5c070f93777fcf0191c26fc07
anasabbas575 <anasabbas503@gmail.com>
2026-03-04 07:21:29 +0500

    Update LAB_2 TASK_7.cpp

--- Lab 2/Lab Task/LAB_2 TASK_7.cpp
+++ Lab 2/Lab Task/LAB_2 TASK_7.cpp
@@ -1,8 +1,7 @@
-// Name: Abdul Ahad
-// Department: Electrical Engineering (AI)
-// Registration No: BF25NWELE0692
-// Section: B
-
+// Name: Anus Abbas Khan
+// Department: Electrical Engineering (power)
+// Registration No: BF25NWELE0723
+// Section: A
 
 
 #include <iostream>   // library for input and output
@@ -18,3 +17,4 @@ int main()
 
     return 0; 
 }
+

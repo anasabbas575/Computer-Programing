1700c5b13d00d537df36dc77fdd12cfd9dfa9040
anasabbas575 <anasabbas503@gmail.com>
2026-04-21 01:25:13 -0700

    Update lab task.cpp

--- Lab 4/LabTask/lab task.cpp
+++ Lab 4/LabTask/lab task.cpp
@@ -8,15 +8,19 @@ using namespace std;
 
 int main()
 {
+    // Declare variables to store user input
     int num1, num2;
     char op;
 
+    // Get numerical input from the user
     cout << "Enter two numbers: ";
     cin >> num1 >> num2;
 
+    // Get the desired operation
     cout << "Enter operator (+, -, *, /): ";
     cin >> op;
 
+    // Evaluate the operator using a switch statement
     switch(op)
     {
         case '+':
@@ -32,16 +36,17 @@ int main()
             break;
 
         case '/':
+            // Check for division by zero before calculating
             if (num2 != 0)
                 cout << "Result = " << num1 / num2;
             else
                 cout << "Error: Division by zero!";
             break;
 
+        // Handle cases where the operator is not recognized
         default:
             cout << "Invalid operator!";
     }
 
     return 0;
-
 }
